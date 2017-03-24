4a5b336c3d515a104a2e26c16088c9a1db45c1cb
Simon Mitchell <simon@3sidedcube.com>
2017-03-24 11:04:39 +0000

    Processes native properties better

--- RNBanner.ios.js
+++ RNBanner.ios.js
@@ -8,21 +8,29 @@ import { processColor } from "react-native";
 
 var NativeRNBanner = require('NativeModules').RNBanner;
 
+function _processProperties(properties) {
+  for (var property in properties) {
+    if (properties.hasOwnProperty(property)) {
+      if (property === 'icon' || property.endsWith('Icon') || property.endsWith('Image')) {
+        properties[property] = resolveAssetSource(properties[property]);
+      }
+      if (property === 'color' || property.endsWith('Color')) {
+        properties[property] = processColor(properties[property]);
+      }
+    }
+  }
+}
+
 /**
  * High-level docs for the RNBanner iOS API can be written here.
  */
-
 export default {
 	show: (title, subtitle, config) => {
 
 		let nativeConfig = {
 			...config
 		}
-
-		if (nativeConfig.backgroundColor) {
-			nativeConfig.backgroundColor = processColor(nativeConfig.backgroundColor)
-		}
-
+		_processProperties(nativeConfig);
 		NativeRNBanner.showToastWithTitle(title, subtitle, nativeConfig);
 	},
 }
